c27f42fc89a30b958407a4aeff34c4a23774bdce
Deivison Johnny <deivisonjohnny@gmail.com>
2024-08-19 21:41:21 -0300

    feat(request): I created the reception of requests, endpoint, function layers for the methods

--- index.php
+++ index.php
@@ -1,2 +1,60 @@
-<php?
+<?php
 
+class Request
+{
+    private $routes;
+    public $method;
+    private $body;
+    private $endpoint;
+
+    function __construct()
+    {
+        $this->routes = [
+            "POST" => [
+                '/auth' => "authUser"
+            ],
+            "GET" => [
+                '/auth' => "authUser",
+            ],
+            "PUT" => [],
+            "DELETE" => [],
+        ];
+
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->endpoint = $_SERVER['REQUEST_URI'];
+
+        $this->handleRequest();
+    }
+
+    function handleRequest()
+    {
+        if (!isset($this->routes[$this->method])) {
+            http_response_code(405);
+            die("Método não permitido");
+        }
+
+        if (!array_key_exists($this->endpoint, $this->routes[$this->method])) {
+            http_response_code(404);
+            die("Endpoint não encontrado");
+        }
+
+        $functionName = $this->routes[$this->method][$this->endpoint];
+
+        if (!method_exists($this, $functionName) && is_callable([$this, $functionName])) {
+            http_response_code(500);
+            die("Erro interno do servidor");
+        }
+
+
+        return $this->$functionName();
+    }
+
+    function authUser(){
+
+        
+
+    }
+
+}
+
+new Request();
